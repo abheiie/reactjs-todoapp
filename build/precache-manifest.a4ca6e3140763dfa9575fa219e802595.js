5d321ddbbba9b3db83e0ca304b6015eef3d4d102
vikas Singh <kpsingh@ftp192168431792121.local>
2021-02-16 15:00:57 +0530

    build version generated

---
+++ build/precache-manifest.a4ca6e3140763dfa9575fa219e802595.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06f47c8a3662c0385027843d75859693",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dc6f42a40e477c8cbfa2",
+    "url": "/static/css/main.27de6b60.chunk.css"
+  },
+  {
+    "revision": "8e49ff261b0b40148c6b",
+    "url": "/static/js/2.c6b53d6d.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.c6b53d6d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc6f42a40e477c8cbfa2",
+    "url": "/static/js/main.d83eb5ab.chunk.js"
+  },
+  {
+    "revision": "ed72fd0300eba1b1f74d",
+    "url": "/static/js/runtime-main.258720a7.js"
+  }
+]);
